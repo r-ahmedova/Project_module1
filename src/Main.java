1f3b4b555b2cfccf78529464ac44b5692530d1cb
r-ahmedova <rayme.ahmedova@abv.bg>
2022-07-07 15:34:51 +0300

    The game without the man

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
         System.out.println("The word has " + randomWord.length() + " letters.");
         char[] letters = new char[randomWord.length()];
         for (int i = 0; i < letters.length; i++) {
-            letters[i] = '.';
+            letters[i] = '-';
         }
-        int lives = 3;
+        int lives = 5;
         Scanner scan = new Scanner(System.in);
         while (lives > 0) {
             System.out.print("Lives : ");
@@ -19,6 +19,40 @@ public class Main {
                 System.out.print(" o ");
             }
             System.out.println();
+            System.out.println("Input : ");
+            String input = scan.nextLine();
+            char letter = input.charAt(0);
+            boolean isGuessCorrect = false;
+            for (int i = 0; i < randomWord.length(); i++) {
+                char l = randomWord.charAt(i);
+                if (l == letter){
+                    letters[i] = l ;
+                    isGuessCorrect = true;
+                }
+            }
+            if (!isGuessCorrect) {
+                lives = lives-1;
+            }
+            boolean isGameFinished = true ;
+            System.out.print("Word : ");
+            for (int i = 0; i < letters.length; i++) {
+                if (letters[i] == '-') {
+                    isGameFinished = false;
+                }
+                System.out.print(letters[i]);
+            }
+            System.out.println();
+            notUsed = notUsed.replace(letter,'.');
+            System.out.println("Not used : " + notUsed);
+            System.out.println("-------");
+            if (isGameFinished) {
+                System.out.println("You won!");
+                break;
+            }
         }
+        if (lives == 0) {
+            System.out.println("You lost ! The word was : " + randomWord);
+        }
+        System.out.println("Exiting game ");
     }
 }
